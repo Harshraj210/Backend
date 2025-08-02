160acc88a8cfc28c468bdb84870562614071d772
harsh <nh1750501@gmail.com>
2025-08-02 22:05:17 +0530

    updated code base

---
+++ 02_Express/crud.js
@@ -0,0 +1,69 @@
+import express from 'express'
+// CRUD --> create,read,update,delete
+
+const app = express()
+// app can understand JSON DATA
+app.use(express.json())
+
+// storing Data in array
+let data = []
+let nextId= 1
+
+// to handle the incoming POST req. --> Sends data to server
+// adding new data
+app.post('/',(req,res)=>{
+  const {name,price}=req.body
+  const newData = {id : nextId++,name,price }
+  data.push(newData)
+  res.status(200).send(newData)
+
+  // it recieves JSON data 
+})
+
+// route to get all data
+app.get('/hello',(req,res)=>{
+  res.status(200).send(data)
+})
+
+// get data with id
+app.get('/hello/:id',(req,res)=>{
+  // to view one item by ID we params
+  const key =data.find(t=> t.id === parseInt(req.params.id))
+  if (!key) {
+    return res.status(404).send("Data not found")
+  }
+  res.status(200).send(key)
+})
+
+// updating the data
+ app.put('/hello/:id',(req,res)=>{
+  
+  const key =data.find(t=> t.id === parseInt(req.params.id))
+
+  if (!key) {
+    return res.status(404).send("Data not found")
+  }
+  const  {name,price} = req.body
+  key.name=name
+  key.price=price
+  res.status(200).send(key)
+ })
+
+//  deleting Data
+
+app.delete('/hello/:id',(req,res)=>{
+  console.log("Delete")
+  console.log(req.params.id)
+  const index=data.findIndex(t => t.id === parseInt(req.params.id))
+
+  if (index === -1) {
+    return res.status(404).send("Key not found")
+  }
+  data.splice(index,1)
+  return res.status(204).send("Data Deleted")
+})
+
+const port = 3000
+app.listen(port,()=>{
+  console.log(`Server is running at : ${port}`)
+})
