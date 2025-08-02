b4fc727274c952d7da252c913b5b1e922a86cb6c
harsh <nh1750501@gmail.com>
2025-08-02 22:57:34 +0530

    added env and updated code base

--- 02_Express/crud.js
+++ 02_Express/crud.js
@@ -1,72 +1,73 @@
-import express from 'express'
+import('dotenv/config')
+import express from "express";
 // CRUD --> create,read,update,delete
 
-const app = express()
+const app = express();
+
 // app can understand JSON DATA
-app.use(express.json())
+app.use(express.json());
 
 // storing Data in array
-let data = []
-let nextId= 1
+let data = [];
+let nextId = 1;
 
 // to handle the incoming POST req. --> Sends data to server
 // adding new data
-app.post('/',(req,res)=>{
-  const {name,price}=req.body
-  const newData = {id : nextId++,name,price }
-  data.push(newData)
-  res.status(200).send(newData)
+app.post("/", (req, res) => {
+  const { name, price } = req.body;
+  const newData = { id: nextId++, name, price };
+  data.push(newData);
+  res.status(200).send(newData);
 
   // it recieves JSON data and sens it back with unique ID
-})
+});
 
 // route to get all data
-app.get('/hello',(req,res)=>{
-  res.status(200).send(data)
-})
+app.get("/hello", (req, res) => {
+  res.status(200).send(data);
+});
 
 // get data with id
-app.get('/hello/:id',(req,res)=>{
+app.get("/hello/:id", (req, res) => {
   // to view one item by ID we params
-  const key =data.find(t=> t.id === parseInt(req.params.id))
+  const key = data.find((t) => t.id === parseInt(req.params.id));
   if (!key) {
-    return res.status(404).send("Data not found")
+    return res.status(404).send("Data not found");
   }
-  res.status(200).send(key)
-})
+  res.status(200).send(key);
+});
 
 // updating the data
- app.put('/hello/:id',(req,res)=>{
-  
-  const key =data.find(t=> t.id === parseInt(req.params.id))
+app.put("/hello/:id", (req, res) => {
+  const key = data.find((t) => t.id === parseInt(req.params.id));
 
   if (!key) {
-    return res.status(404).send("Data not found")
+    return res.status(404).send("Data not found");
   }
-  const  {name,price} = req.body
-  key.name=name
-  key.price=price
-  res.status(200).send(key)
+  const { name, price } = req.body;
+  key.name = name;
+  key.price = price;
+  res.status(200).send(key);
 
   // we send the updated Data withy name and price , it changes with the given ID
- })
+});
 
 //  deleting Data
 // to delete first we have to find Id and remove it with ID
 
-app.delete('/hello/:id',(req,res)=>{
-  console.log("Delete")
-  console.log(req.params.id)
-  const index=data.findIndex(t => t.id === parseInt(req.params.id))
+app.delete("/hello/:id", (req, res) => {
+  console.log("Delete");
+  console.log(req.params.id);
+  const index = data.findIndex((t) => t.id === parseInt(req.params.id));
 
   if (index === -1) {
-    return res.status(404).send("Key not found")
+    return res.status(404).send("Key not found");
   }
-  data.splice(index,1)
-  return res.status(204).send("Data Deleted")
-})
+  data.splice(index, 1);
+  return res.status(204).send("Data Deleted");
+});
 
-const port = 3000
-app.listen(port,()=>{
-  console.log(`Server is running at : ${port}`)
-})
+const port = process.env.PORT ||3000;
+app.listen(port, () => {
+  console.log(`Server is running at : ${port}`);
+});
